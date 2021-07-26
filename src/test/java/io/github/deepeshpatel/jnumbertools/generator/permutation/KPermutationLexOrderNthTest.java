1526a8d83020eab1bd07a40d3c614f1658bcfb16
Deepesh <patel.deepesh@gmail.com>
2021-07-26 13:32:59 +0530

    Optimization of permutational number system

--- src/test/java/io/github/deepeshpatel/jnumbertools/generator/permutation/KPermutationLexOrderNthTest.java
+++ src/test/java/io/github/deepeshpatel/jnumbertools/generator/permutation/KPermutationLexOrderNthTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class KPermutationLexOrderNthTest {
 
